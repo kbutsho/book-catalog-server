97f9e4d5decb1a6f13a99582c07df85ff16a0795
Kaushik Biswas <kbutsho@gmail.com>
2023-07-15 13:20:39 +0600

    book constant added

---
+++ src/app/modules/books/book.constant.ts
@@ -0,0 +1,2 @@
+export const bookSearchableFields = ['title', 'author', 'genre', 'publicationDate'];
+export const bookFilterableFields = ['searchTerm', 'title', 'author', 'genre', 'publicationDate'];
